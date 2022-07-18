9e89c7e7ecd9039c05d90ae5cd23e990b2821fc5
houhong <houhong@tydic.com>
2022-07-19 01:23:52 +0800

    -- 初始化bpm-monitor 监控平台

---
+++ bpm-monitor/src/main/java/com/houhong/bpmmonitor/BpmMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.houhong.bpmmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpmMonitorApplication.class, args);
+    }
+
+}
---
+++ bpm-monitor/src/test/java/com/houhong/bpmmonitor/BpmMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houhong.bpmmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpmMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
