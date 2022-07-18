3868356e4c519fc115748abe1d4dc488e7ee95ea
houhong <houhong@tydic.com>
2022-07-19 01:21:33 +0800

    -- 初始化bpm-mq-starter

---
+++ bpm-mq-springboot-starter/src/main/java/com/houhong/bpmmqspringbootstarter/BpmMqSpringbootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.houhong.bpmmqspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmMqSpringbootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpmMqSpringbootStarterApplication.class, args);
+    }
+
+}
---
+++ bpm-mq-springboot-starter/src/test/java/com/houhong/bpmmqspringbootstarter/BpmMqSpringbootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houhong.bpmmqspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpmMqSpringbootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
