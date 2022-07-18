7eeba9122b15a76a022a119b92408968ca89c32d
houhong <houhong@tydic.com>
2022-07-19 01:18:29 +0800

    -- 提供demo 案例

---
+++ bpm-boot-samples/src/main/java/com/houhong/bpmbootsamples/BpmBootSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.houhong.bpmbootsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmBootSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpmBootSamplesApplication.class, args);
+    }
+
+}
---
+++ bpm-boot-samples/src/test/java/com/houhong/bpmbootsamples/BpmBootSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houhong.bpmbootsamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpmBootSamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
